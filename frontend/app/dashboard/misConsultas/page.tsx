7a7673ba4d32790abc534cef1c7af7a44df7fb6b
AndersonJimenez <anjimhol23@gmail.com>
2024-05-01 15:14:29 -0500

    guardando cambios

--- frontend/app/dashboard/misConsultas/page.tsx
+++ frontend/app/dashboard/misConsultas/page.tsx
@@ -7,6 +7,7 @@ import { SioNo } from "@/app/ui/modals/SioNo";
 
 
 
+
 export default function misConsultas() {
   const [isMessageVisible, setIsMessageVisible] = useState(true);
   const [mensaje, setMensaje] = useState("");
@@ -47,6 +48,7 @@ export default function misConsultas() {
               )
             }
           />
+         
         </section>
       </div>
     </>
